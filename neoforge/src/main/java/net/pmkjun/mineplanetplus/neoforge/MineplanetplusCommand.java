06011b1295bd9d0ecc5a71657f7bfae7a51c1164
junhuk1113 <junhuk1113@naver.com>
2025-09-04 23:15:38 +0900

    네오포지 코드 작업2

--- neoforge/src/main/java/net/pmkjun/mineplanetplus/neoforge/MineplanetplusCommand.java
+++ neoforge/src/main/java/net/pmkjun/mineplanetplus/neoforge/MineplanetplusCommand.java
@@ -60,7 +60,9 @@ public class MineplanetplusCommand {
     }
 
     private static int loadNearTotem(CommandContext<CommandSourceStack> context){
-        context.getSource().sendSystemMessage(Component.literal("↓ 주변에 있는 토템 ↓\n").withStyle(Style.EMPTY.withColor(0x84CA77)).append(TotemList.getComponent()));
+        context.getSource().sendSystemMessage(Component.literal("↓ 주변에 있는 토템 ↓ ").withStyle(Style.EMPTY.withColor(0x84CA77))
+                .append(Component.literal("(마우스를 올려 토템 스펙 확인)\n").withStyle(Style.EMPTY.withColor(0xCAD1E0)))
+                .append(TotemList.getComponent()));
         return 1;
     }
 }
