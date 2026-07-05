0fe940fd0294eedc1d5a7ae5b6f010bff2260078
mighty_Jun <junhuk1113@naver.com>
2026-07-05 12:23:50 +0900

    Inital commit for 26.1.2

---
+++ common/src/main/java/net/pmkjun/mineplanetplus/Constants.java
@@ -0,0 +1,11 @@
+package net.pmkjun.mineplanetplus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "mineplanetplus";
+	public static final String MOD_NAME = "MineplanetPlus";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/net/pmkjun/mineplanetplus/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package net.pmkjun.mineplanetplus.platform;
+
+import net.pmkjun.mineplanetplus.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
